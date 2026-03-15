9fcf97cb40813b61c970fcf48fa316fa94619b8e
Xinrui Chen <xynree@gmail.com>
2026-03-14 21:24:53 -0700

    rename manager to base (#1)

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { app, BrowserWindow } from "electron";
 import path from "node:path";
 import started from "electron-squirrel-startup";
 import { initialize } from "./main/initialize";
-import { FolioManager } from "./main/manager";
+import { FolioManager } from "./main/base.manager";
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
